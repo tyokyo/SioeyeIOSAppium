f3e348bd38a18c52590b669d8fe62f362c7dbb46
qiang.zhang <qiang.zhang@ck-telecom.com>
2017-02-21 17:13:41 +0800

    e

---
+++ src/ckt/App/Util/IElement.java
@@ -0,0 +1,81 @@
+package ckt.App.Util;
+
+public class IElement {
+		private String className;
+		public String getClassName() {
+			return className;
+		}
+		public void setClassName(String className) {
+			this.className = className;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getLabel() {
+			return label;
+		}
+		public void setLabel(String label) {
+			this.label = label;
+		}
+		public String getValue() {
+			return value;
+		}
+		public void setValue(String value) {
+			this.value = value;
+		}
+		public String getEnabled() {
+			return enabled;
+		}
+		public void setEnabled(String enabled) {
+			this.enabled = enabled;
+		}
+		public String getVisible() {
+			return visible;
+		}
+		public void setVisible(String visible) {
+			this.visible = visible;
+		}
+		public String getXpath() {
+			return xpath;
+		}
+		public void setXpath(String xpath) {
+			this.xpath = xpath;
+		}
+		public double getX() {
+			return x;
+		}
+		public void setX(double x) {
+			this.x = x;
+		}
+		public double getY() {
+			return y;
+		}
+		public void setY(double y) {
+			this.y = y;
+		}
+		public double getWidth() {
+			return width;
+		}
+		public void setWidth(double width) {
+			this.width = width;
+		}
+		public double getHeight() {
+			return height;
+		}
+		public void setHeight(double height) {
+			this.height = height;
+		}
+		private String name;
+		private String label;
+		private String value;
+		private String enabled;
+		private String visible;
+		private String xpath;
+		private double x;
+		private double y;
+		private double width;
+		private double height;
+}
