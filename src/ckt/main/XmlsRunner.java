4baac5680a083699c5b513dc49f5ed6dbdcf3b85
qiang.zhang <qiang.zhang@ck-telecom.com>
2017-04-13 17:22:00 +0800

    test

--- src/ckt/main/XmlsRunner.java
+++ src/ckt/main/XmlsRunner.java
@@ -21,13 +21,14 @@ public class XmlsRunner {
 		testNG.addListener(new TestngListener());
 		List<String> suits = new ArrayList<String>();
 		//添加测试套件-读取XML文件夹下所有的.xml文件（testng 配置文件）
-		File xmlDir=new File("xml");
+		File xmlDir=new File("xml5");
 		File[] xmlFiles=xmlDir.listFiles();
 		for (File file: xmlFiles) {
 			if (file.getName().toLowerCase().endsWith(".xml")){
 				suits.add(file.getAbsolutePath());
 			}
 		}
+		testNG.setTestSuites(suits);
 		testNG.run();
 	}
 }
